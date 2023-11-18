f19b7e73501bbb6231ebb9a88614dbf860406f41
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2023-11-18 17:51:28 +0530

    update/add functionality of del/restore button
    -handle event mouseover & mouseout for restore button(use same function of
    del button)
    -update functionality of delete button i.e first add in 'completed-tasks' & then remove
    -add functionality to restore button i.e first add in 'user-tasks' and
    then remove

--- app.js
+++ app.js
@@ -20,35 +20,52 @@ document.addEventListener("keypress", function (eve) {
 
 // mouseover and mouseout on delete button's common ancestor
 let list = document.querySelector("#items ul");
-list.addEventListener("mouseover", function(event){
+let listOfComp = document.querySelector("#items-deleted ul");
+function mouseoverFn(event){
     if(event.target.className == "item"){
         event.target.children[0].style.display = "inline";
     }
-    else if(event.target.className == "del"){
+    else if(event.target.className == "del" || event.target.className == "res"){
         event.target.style.display = "inline";
     }
     else if(event.target.tagName == "BUTTON"){
         event.target.parentElement.style.display = "inline";
     }
-});
-list.addEventListener("mouseout", function(event){
+}
+function mouseoutFn(event){
     if(event.target.className == "item"){
         event.target.children[0].style.display = "none";
     }
-    else if(event.target.className == "del" && event.toElement.id == "items"){
+    else if((event.target.className == "del" && event.toElement.id == "items") || (event.target.className == "res" && event.toElement.id == "items-deleted")) {
         event.target.style.display = "none";
     }
-    else if(event.target.className == "del"){
+    else if(event.target.className == "del" || event.target.className == "res"){
         event.target.style.display = "inline";
     }
     else if(event.target.tagName == "BUTTON"){
         event.target.parentElement.style.display = "inline";
     }
-});
+}
+list.addEventListener("mouseover", mouseoverFn);
+listOfComp.addEventListener("mouseover", mouseoverFn);
+list.addEventListener("mouseout", mouseoutFn);
+listOfComp.addEventListener("mouseout", mouseoutFn);
 
 // Delete button functionality
+function addItemInCompleted(task) {
+    let li = document.createElement("li");
+    li.classList.add("item");
+    li.innerHTML = `${task}<span class="res"><button>restore</button></span>`
+
+    document.querySelector("#items-deleted ul").append(li);
+    newItem.value = "";
+    console.log(li, "completed");
+}
 list.addEventListener("click", function(e) {
     if(e.target.tagName == "BUTTON"){
+        let txt = e.target.parentElement.parentElement.innerText;
+        txt = txt.replaceAll(/\ndelete/g, "");
+        addItemInCompleted(txt);
         e.target.parentElement.parentElement.remove();
     }
 });
@@ -59,8 +76,6 @@ let itemsDel = document.querySelector("div#items-deleted");
 let theSwitch = document.querySelector("#switch");
 
 theSwitch.addEventListener("click", function(event) {
-    console.dir(event);
-    console.dir(itemsDel.style);
     if(itemsDel.style.display == "none"){
         items.style.display = "none";
         itemsDel.style.display = "block";
@@ -73,5 +88,24 @@ theSwitch.addEventListener("click", function(event) {
         itemsDel.style.display = "none";
         theSwitch.innerText = "Completed tasks";
         newItem.disabled = false;
+    }
+});
+
+// Restore button functionality
+function restore(task) {
+    let li = document.createElement("li");
+    li.classList.add("item");
+    li.innerHTML = `${task}<span class="del"><button>delete</button></span>`
+
+    document.querySelector("#items ul").append(li);
+    newItem.value = "";
+    console.log(li, "restored");
+}
+listOfComp.addEventListener("click", function(e) {
+    if(e.target.tagName == "BUTTON"){
+        let txt = e.target.parentElement.parentElement.innerText;
+        txt = txt.replaceAll(/\nrestore/g, "");
+        restore(txt);
+        e.target.parentElement.parentElement.remove();
     }
 });
